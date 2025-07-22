7b2b0293c9be2d87175d9f21969eae1d4aa5a80c
Will Herzog <willherzog@gmail.com>
2025-07-22 14:46:02 -0600

    Add interface description and @author declaration

--- src/View/UseSortByPropertyForRequests.php
+++ src/View/UseSortByPropertyForRequests.php
@@ -2,6 +2,13 @@
 
 namespace WHSymfony\WHItemIndexTableBundle\View;
 
+/**
+ * Create a class extending from {@link WHSymfony\WHItemIndexTableBundle\View\ItemTableColumn} that
+ * implements this interface to opt into using the sort-by property value of each such column for
+ * request queries (instead of the slug value, which is the default behavior).
+ *
+ * @author Will Herzog <willherzog@gmail.com>
+ */
 interface UseSortByPropertyForRequests
 {
 }
